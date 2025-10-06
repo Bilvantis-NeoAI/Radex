a2393db74823d0f0e6b052cdd3948e793124015d
Krish Hindocha <krish.hindocha@bilvantis.io>
2025-10-06 15:30:52 +0530

    Update utils.ts

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,7 +1,7 @@
 import { twMerge } from 'tailwind-merge';
-import clsx from 'clsx';
+import clsx, { type ClassValue } from 'clsx';
 
-export function cn(...inputs: any[]): string {
+export function cn(...inputs: ClassValue[]): string {
 	return twMerge(clsx(inputs));
 }
 
